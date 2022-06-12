fd2a0ce8fd65825f7aeab7ddd45c2929b2507465
Peter Nguyen (Win) <pterl.nguyen@gmail.com>
2022-06-12 00:00:16 -0400

    add meetRooms2

---
+++ blind75/python/MeetingRoomsII.py
@@ -0,0 +1,44 @@
+class LC:
+    """
+    Desc:
+        # 253
+        Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+    Link: 
+        https://leetcode.com/problems/meeting-rooms-ii/
+    Notes:
+    """
+
+    # create list of start times and end times
+        # sort them
+        # traverse them in order, 
+        # start time mean rooms is need +1, end time means room is open -1
+        # track max rooms needed till start times empty, no more start times means no more rooms needed
+    # Time: O(nlogn) - sorting
+    # Space: O(n) - creating 2 new list of size n
+    def solution1(self, intervals):
+        res, rooms = 0, 0
+        
+        startTimes, endTimes = [], []
+        for start, end in intervals:
+            startTimes.append(start)
+            endTimes.append(end)
+        startTimes.sort()
+        endTimes.sort()
+        
+        while startTimes:
+            if startTimes[0] < endTimes[0]:
+                startTimes.pop(0)
+                rooms += 1
+                res = max(res, rooms)
+            else:
+                endTimes.pop(0)
+                rooms -= 1
+            
+        return res 
+
+
+    # priority queues
+    # Time: O()
+    # Space: O()
+    def solution2():
+        pass
